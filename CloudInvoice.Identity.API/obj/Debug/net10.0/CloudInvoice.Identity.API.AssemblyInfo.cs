1d6b75496747bca01d4fc32fe6c2c057c8c6947f
Nuno <267765083+nunoQueirosCode@users.noreply.github.com>
2026-07-21 03:35:56 +0100

    fix: forca o git a esquecer ficheiros obj e bin rastreados indevidamente

--- CloudInvoice.Identity.API/obj/Debug/net10.0/CloudInvoice.Identity.API.AssemblyInfo.cs
+++ CloudInvoice.Identity.API/obj/Debug/net10.0/CloudInvoice.Identity.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudInvoice.Identity.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c25441a8fb0d84a766edd3b0aeaff5cecc0f140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cd2f184a54f70895bec05e71a9b1ee83486eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudInvoice.Identity.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudInvoice.Identity.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
